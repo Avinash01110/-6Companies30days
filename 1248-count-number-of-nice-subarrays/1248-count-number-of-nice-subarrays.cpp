4adb369d83fd635c287e0c1a258039c47cc57bf7
Avinash Gupta <123259868+Avinash01110@users.noreply.github.com>
2025-01-05 16:38:36 +0530

    Time: 71 ms (23.38%), Space: 94.2 MB (17.58%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+
+    int prefixSum(vector<int> &nums, int k){
         int n = nums.size();
+
+        int oddCnt = 0;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
 
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int cnt = k;
-                for (int x = i; x <= j; x++) {
-
-                    if (nums[x] % 2 != 0) {
-                        cnt--;
-                    }
-                }
-                if (!cnt) {
-                    ans++;
-                }
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] % 2 != 0){
+                oddCnt++;
             }
+
+            if(mp.count(oddCnt - k)){
+                ans += mp[oddCnt - k];
+            }
+
+            mp[oddCnt]++;
         }
+
+        return ans;
+
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int ans = prefixSum(nums, k);
 
         return ans;
     }
